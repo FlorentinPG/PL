9255030dde6da7f21e24e93521156a17538117d6
Florentín Pérez González <alu0101100654@ull.edu.es>
2020-02-08 16:27:59 +0000

    Añadidas funcionalidades

--- logging-espree.js
+++ logging-espree.js
@@ -1,6 +1,7 @@
 const escodegen = require('escodegen');
 const espree = require('espree');
 const estraverse = require('estraverse');
+const fs = require('fs');
 
 function addLogging(code) {
     const ast = espree.parse(code);
@@ -18,8 +19,8 @@ function addLogging(code) {
 function addBeforeCode(node) {
     const name = node.id ? node.id.name : '<anonymous function>';
     const argumentsData = getArgumentsData(node.params);
-    const beforeCode = "console.log('Entering " + name + argumentsData + "');";
-    const beforeNodes = espree.parse(beforeCode).body;
+    const beforeCode = "console.log(`Entering " + name + argumentsData + "`);";
+    const beforeNodes = espree.parse(beforeCode, {ecmaVersion:6}).body;
     node.body.body = beforeNodes.concat(node.body.body);
 }
 
@@ -28,18 +29,51 @@ function getArgumentsData(node) {
     for(let i = 0; i < node.length; i++) {
         data = data + "${ " + `${node[i].name}` + " }"
         if (i+1 < node.length) {
-            data += ",";
+            data += ", ";
         }
     }
     return data;
 }
 
-console.log(addLogging(`
-function foo(a, b) {   
-  var x = 'blah';   
-  var y = (function (z) {
-    return 3;
-  })();
+var helpNeeded = false;
+var inputFile = "";
+var outputFile = "out.js";
+var isUnkown = false;
+for (var i = 2; i < process.argv.length; i++) {
+    switch (process.argv[i]) {
+        case '-i':
+        case '--input':
+            inputFile = process.argv[++i];
+            break;
+        case '-o':
+        case '--output':
+            outputFile = process.argv[++i];
+            break;
+        case '-h':
+        case '--help':
+            helpNeeded = true;
+            break;
+        default:
+            console.log("Unkown option detected");
+            isUnkown = true;
+            break;
+    }
 }
-foo(1, 'wut', 3);
-`));
+
+if(helpNeeded || process.argv.length == 2 || isUnkown) {
+    console.log(`Usage: logging-espree.js [options]\n
+Options:\n
+    -i, --input <filename>\t\tSpecify the input file.\n
+    -o, --output <filename>\t\tSpecify the output file. By defect, "out.js".\n
+    -h, --help\t\t\t\tOutput usage information.`)
+}else if (!inputFile) {
+    console.log(`You have to specify an input file. Check the program help with -h or the --help options to know more about.`)
+}else {
+    let archivo = fs.readFileSync(inputFile, 'utf-8');
+    fs.writeFile(outputFile,addLogging(archivo), error => {
+        if (error)
+            console.log(error);
+        else 
+            console.log(`The output file, "${outputFile}", has been created successfully.`);
+    });
+}
